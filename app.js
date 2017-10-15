d8445d7392b3c0ef5a650da85387e1b8915c9dc0
jiaminchen <jmchen.ggm@gmail.com>
2017-10-15 17:09:00 +0800

    初次上传项目

---
+++ app.js
@@ -0,0 +1,16 @@
+//app.js
+App({
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+  globalData: {
+    hasLogin: false,
+    openid: "wxf988e95addc14836"
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,57 @@
+Page({
+  data :{
+    menu1: [
+      {
+        id : "safe-vaulg",
+        img: "/resources/common/img/menu-item-1.png",
+        title: "保险库",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "rank",
+        img: "/resources/common/img/menu-item-2.png",
+        title: "保险排名",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "compare",
+        img: "/resources/common/img/menu-item-3.png",
+        title: "保险对比",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "analyse",
+        img: "/resources/common/img/menu-item-4.png",
+        title: "保单分析",
+        page: "/pages/analyse/analyse"
+      }
+    ],
+    menu2: [
+      {
+        id: "book",
+        img: "/resources/common/img/menu-item-5.png",
+        title: "保险定制",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "insurace",
+        img: "/resources/common/img/menu-item-6.png",
+        title: "带病投保",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "settlement",
+        img: "/resources/common/img/menu-item-7.png",
+        title: "理赔通",
+        page: "/pages/safe-vault/safe-vault"
+      },
+      {
+        id: "all",
+        img: "/resources/common/img/menu-item-8.png",
+        title: "全部",
+        page: "/pages/safe-vault/safe-vault"
+      }
+    ]
+  }
+}
+)
