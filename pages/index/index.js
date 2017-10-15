ddc2f6b7bfe199404ef9639fce6af2ad5fad2f45
jiaminchen <jmchen.ggm@gmail.com>
2017-10-15 20:22:52 +0800

    修改保险库名字

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,19 +5,19 @@ Page({
         id : "safe-vaulg",
         img: "/resources/common/img/menu-item-1.png",
         title: "保险库",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "rank",
         img: "/resources/common/img/menu-item-2.png",
         title: "保险排名",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "compare",
         img: "/resources/common/img/menu-item-3.png",
         title: "保险对比",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "analyse",
@@ -31,25 +31,25 @@ Page({
         id: "book",
         img: "/resources/common/img/menu-item-5.png",
         title: "保险定制",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "insurace",
         img: "/resources/common/img/menu-item-6.png",
         title: "带病投保",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "settlement",
         img: "/resources/common/img/menu-item-7.png",
         title: "理赔通",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       },
       {
         id: "all",
         img: "/resources/common/img/menu-item-8.png",
         title: "全部",
-        page: "/pages/safe-vault/safe-vault"
+        page: "/pages/insurances/insurances"
       }
     ]
   }
